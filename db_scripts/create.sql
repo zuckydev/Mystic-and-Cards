73a5dfda63fa6485e23ac2abb8a5f4dc94d191fc
zuckydev <sofialeite.panda@gmail.com>
2023-04-13 18:01:11 +0100

    Database

--- db_scripts/create.sql
+++ db_scripts/create.sql
@@ -1,6 +1,8 @@
-create database mygame;
+drop database macdb;
 
-use mygame;
+create database macdb;
+
+use macdb;
 
 create table user (
     usr_id int not null auto_increment,
@@ -26,6 +28,8 @@ create table user_game (
     ug_user_id int not null,
     ug_game_id int not null,
     ug_state_id int not null,
+    ug_gold int not null default 10,
+    ug_mine_level int not null default 0,
     primary key (ug_id));
 
 create table user_game_state (
@@ -34,17 +38,82 @@ create table user_game_state (
     primary key (ugst_id));
 
 
-create table scoreboard (
-    sb_id int not null auto_increment,
-    sb_user_game_id int not null,
-    sb_state_id int not null,
-    sb_points int not null,
-    primary key (sb_id));
+# create table scoreboard (
+#     sb_id int not null auto_increment,
+#     sb_user_game_id int not null,
+#     sb_state_id int not null,
+#     sb_points int not null,
+#     primary key (sb_id));
 
- create table scoreboard_state (
-    sbs_id int not null auto_increment,
-    sbs_state varchar(60) not null,
-    primary key (sbs_id));
+#  create table scoreboard_state (
+#     sbs_id int not null auto_increment,
+#     sbs_state varchar(60) not null,
+#     primary key (sbs_id));
+
+create table rarity (
+    rar_id int not null auto_increment,
+    rar_name varchar(60) not null,
+    primary key (rar_id));
+
+# create table deck (
+#    dec_id int not null auto_increment,
+#    dec_rarity_id int not null,
+#    primary key (dec_id));
+
+create table card (
+    crd_id int not null auto_increment,
+    crd_name varchar(60) not null,
+    crd_rarity int not null,
+    primary key (crd_id));
+
+create table card_attack (
+	ctk_id int not null auto_increment,
+	ctk_attack int not null,
+	ctk_hp int not null,
+	ctk_crd_id int not null,
+	primary key (ctk_id));
+
+create table card_shield (
+	csh_id int not null auto_increment,
+	csh_hp int not null,
+	csh_crd_id int not null,
+	primary key (csh_id));
+
+create table card_spell (
+	csp_id int not null auto_increment,
+	csp_attack int not null,
+	csp_crd_id int not null,
+	primary key (csp_id));
+
+# create table deck_has_card (
+#    dhc_dec_id int not null,
+#    dhc_crd_id int not null,
+#    primary key (dhc_dec_id, dhc_crd_id));
+
+create table user_game_card_state (
+    ucs_id int not null auto_increment,
+    ucs_state varchar(60) not null,
+    primary key (ucs_id));
+
+create table user_game_card (
+    ugc_id int not null auto_increment,
+    ugc_crd_id int not null,
+    ugc_state_id int not null,
+    ugc_user_game_id int not null,
+    ugc_position int not null,
+    primary key (ugc_id));
+
+# create table user_game_deck (
+#    ugd_id int not null auto_increment,
+#    ugd_user_game_id int not null,
+#    ugd_rar_id int not null,
+#    primary key (ugd_id));
+
+# create table user_game_deck_cards (
+#    udc_id int not null auto_increment,
+#    udc_ugc_id int not null,
+#    udc_ugd_id int not null,
+#    primary key (udc_id));
 
 
 # Foreign Keys
@@ -65,10 +134,80 @@ alter table user_game add constraint user_game_fk_user_game_state
             foreign key (ug_state_id) references user_game_state(ugst_id) 
 			ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-alter table scoreboard add constraint scoreboard_fk_user_game
-            foreign key (sb_user_game_id) references user_game(ug_id) 
-			ON DELETE NO ACTION ON UPDATE NO ACTION;  
+# alter table scoreboard add constraint scoreboard_fk_user_game
+#            foreign key (sb_user_game_id) referencudc_ugc_ides user_game(ug_id) 
+#			ON DELETE NO ACTION ON UPDATE NO ACTION;  
 
-alter table scoreboard add constraint scoreboard_fk_scoreboard_state
-            foreign key (sb_state_id) references scoreboard_state(sbs_id) 
-			ON DELETE NO ACTION ON UPDATE NO ACTION;  
+# alter table scoreboard add constraint scoreboard_fk_scoreboard_state
+#            foreign key (sb_state_id) references scoreboard_state(sbs_id) 
+#			ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+# --- Template cards ---
+
+alter table card_attack add constraint card_attack_fk_card
+            foreign key (ctk_crd_id) references card(crd_id) 
+			ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+alter table card_shield add constraint card_shield_fk_card
+            foreign key (csh_crd_id) references card(crd_id)
+			ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+alter table card_spell add constraint card_spell_fk_card
+            foreign key (csp_crd_id) references card(crd_id) 
+            ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+# --- Template deck ---
+
+# alter table deck add constraint deck_fk_rarity
+#            foreign key (dec_rarity_id) references rarity(rar_id)
+#            ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+# --- Deck to card connection ---
+
+# alter table deck_has_card add constraint deck_has_card_fk_deck
+#            foreign key (dhc_dec_id) references deck(dec_id) 
+#			ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+# alter table deck_has_card add constraint deck_has_card_fk_card
+#            foreign key (dhc_crd_id) references card(crd_id) 
+#			ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+# --- User Game Card ---
+
+alter table user_game_card add constraint user_game_card_fk_card
+            foreign key (ugc_crd_id) references card(crd_id) 
+			ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+alter table user_game_card add constraint user_game_card_fk_user_game_card_state
+            foreign key (ugc_state_id) references user_game_card_state(ucs_id)
+            ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+alter table user_game_card add constraint user_game_card_fk_user_game
+            foreign key (ugc_user_game_id) references user_game(ug_id)
+            ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+# --- User Game Deck ---
+
+# alter table user_game_deck add constraint user_game_deck_fk_user
+#            foreign key (ugd_user_game_id) references user_game(ug_id) 
+#			ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+# alter table user_game_deck add constraint user_game_deck_fk_rarity
+#            foreign key (ugd_rar_id) references rarity(rar_id)
+#			ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+# --- User Game Deck Cards ---
+
+# alter table user_game_deck_cards add constraint user_game_deck_cards_fk_user_game_deck
+#            foreign key (udc_ugd_id) references user_game_deck (ugd_id)
+#            ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+# alter table user_game_deck_cards add constraint user_game_deck_cards_fk_user_game_card
+#            foreign key (udc_ugc_id) references user_game_card (ugc_id)
+#            ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+# -- new stuff --
+
+alter table card add constraint card_fk_rarity
+            foreign key (crd_rarity) references rarity(rar_id)
+            ON DELETE NO ACTION ON UPDATE NO ACTION;
--- db_scripts/populate.sql
+++ db_scripts/populate.sql
@@ -13,9 +13,189 @@ insert into user_game_state (ugst_state) values ('Score');
 insert into user_game_state (ugst_state) values ('End');
 
 # Possible end game states
-insert into scoreboard_state (sbs_state) values ('Tied');
-insert into scoreboard_state (sbs_state) values ('Lost');
-insert into scoreboard_state (sbs_state) values ('Won');
+# insert into scoreboard_state (sbs_state) values ('Tied');
+# insert into scoreboard_state (sbs_state) values ('Lost');
+# insert into scoreboard_state (sbs_state) values ('Won');
+
+insert into rarity (rar_name) values
+    ("Common"),
+    ("Epic"),
+    ("Legendary");
+
+insert into user_game_card_state (ucs_state) values
+#    ("Deck"),
+    ("Hand"),
+    ("Zone"),
+    ("Discarded");
+
+# insert into deck (dec_rarity_id) values
+#    (1),
+#    (2),
+#    (3);
+
+insert into card (crd_name, crd_rarity) values
+    ("Rat", 1),
+    ("Zombie", 1),
+    ("Rock", 1),
+    ("Spider", 1),
+    ("Skeleton", 1),
+    ("Kraken", 2),
+    ("Reaper", 2),
+    ("Golem", 2),
+    ("Hydra", 3),
+    ("Demon", 3),
+
+    ("Wood Shield", 1),
+    ("Wind Barrier", 1),
+    ("Knight's Shield", 1),
+    ("Steel Prison", 2),
+    ("Diamond Wall", 2),
+    ("Holy Protector", 3),
+    ("Demon's King Guard", 3),
+
+    ("Iceball", 1),
+    ("Fireball", 1),
+    ("Wind Blade", 1),
+    ("Poison Cloud", 2),
+    ("Thunderbolt", 2),
+    ("Meteor", 3);
 
 
+insert into card_attack (ctk_attack, ctk_hp, ctk_crd_id) values
+    (2, 2, 1),
+    (3, 1, 2),
+    (0, 6, 3),
+    (3, 3, 4),
+    (4, 2, 5),
+    (4, 4, 6),
+    (8, 2, 7),
+    (2, 12, 8),
+    (7, 8, 9),
+    (10, 5, 10);
 
+insert into card_shield (csh_hp, csh_crd_id) values
+    (1, 11),
+    (2, 12),
+    (3, 13),
+    (4, 14),
+    (5, 15),
+    (7, 16),
+    (7, 17);
+
+insert into card_spell (csp_attack, csp_crd_id) values
+    (1, 18),
+    (2, 19),
+    (3, 20),
+    (4, 21),
+    (6, 22),
+    (8, 23);
+=======
+insert into user_game_card_state (ucs_state) values
+    ("Deck"),
+    ("Hand"),
+    ("Zone"),
+    ("Discarded");
+
+insert into deck (dec_rarity_id) values
+    (1),
+    (2),
+    (3);
+
+insert into card (crd_name) values
+    ("Zombie");
+insert into deck_has_card (dhc_crd_id, dhc_dec_id) values
+    (1, 1);
+
+insert into card (crd_name) values
+    ("Skeleton");
+insert into deck_has_card (dhc_crd_id, dhc_dec_id) values
+    (2, 1);
+
+insert into card (crd_name) values
+    ("Kraken");
+insert into deck_has_card (dhc_crd_id, dhc_dec_id) values
+    (3, 2);
+
+insert into card (crd_name) values
+    ("Golem");
+insert into deck_has_card (dhc_crd_id, dhc_dec_id) values
+    (4, 2);
+
+insert into card (crd_name) values
+    ("Demon");
+insert into deck_has_card (dhc_crd_id, dhc_dec_id) values
+    (5, 3);
+
+
+insert into card (crd_name) values
+    ("Wood Shield");
+insert into deck_has_card (dhc_crd_id, dhc_dec_id) values
+    (6, 1);
+
+insert into card (crd_name) values
+    ("Knight's Shield");
+insert into deck_has_card (dhc_crd_id, dhc_dec_id) values
+    (7, 1);
+
+insert into card (crd_name) values
+    ("Steel Prison");
+insert into deck_has_card (dhc_crd_id, dhc_dec_id) values
+    (8, 2);
+
+insert into card (crd_name) values
+    ("Diamond Wall");
+insert into deck_has_card (dhc_crd_id, dhc_dec_id) values
+    (9, 2);
+
+insert into card (crd_name) values
+    ("Holy Protector");
+insert into deck_has_card (dhc_crd_id, dhc_dec_id) values
+    (10, 3);
+
+
+insert into card (crd_name) values
+    ("Iceball");
+insert into deck_has_card (dhc_crd_id, dhc_dec_id) values
+    (11, 1);
+
+insert into card (crd_name) values
+    ("Fireball");
+insert into deck_has_card (dhc_crd_id, dhc_dec_id) values
+    (12, 1);
+
+insert into card (crd_name) values
+    ("Poison Cloud");
+insert into deck_has_card (dhc_crd_id, dhc_dec_id) values
+    (13, 2);
+
+insert into card (crd_name) values
+    ("Thunderbolt");
+insert into deck_has_card (dhc_crd_id, dhc_dec_id) values
+    (14, 2);
+
+insert into card (crd_name) values
+    ("Meteor");
+insert into deck_has_card (dhc_crd_id, dhc_dec_id) values
+    (15, 3);
+
+
+insert into card_attack (ctk_attack, ctk_hp, ctk_crd_id) values
+    (3, 1, 1),
+    (4, 2, 2),
+    (6, 4, 3),
+    (2, 12, 4),
+    (10, 5, 5);
+
+insert into card_shield (csh_hp, csh_crd_id) values
+    (1, 6),
+    (3, 7),
+    (4, 8),
+    (5, 9),
+    (7, 10);
+
+insert into card_spell (csp_attack, csp_crd_id) values
+    (1, 11),
+    (2, 12),
+    (4, 13),
+    (6, 14),
+    (8, 15);
